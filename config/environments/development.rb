11cb181467d268e3b4e580ab44e3c9203d823132
Ozzie_Gooen@hmc.edu <ozziegooen@wl-201-114.cine.hmc.edu>
2010-11-26 03:53:05 -0800

    Footer added and stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Pdb::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
+  Paperclip.options[:image_magick_path] = '/Downloads/ImageMagick-6.6.5/bin'
 
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
---
+++ db/migrate/20101117044448_create_projects.rb
@@ -0,0 +1,17 @@
+class CreateProjects < ActiveRecord::Migration
+  def self.up
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.string :kind
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_index :projects, :user_id
+  end
+
+  def self.down
+    drop_table :projects
+  end
+end
