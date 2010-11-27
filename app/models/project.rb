7f7e62d776238d5c3f0901777891b69f08613970
RAHUL <rahul@rahul-VirtualBox.(none)>
2010-11-26 20:07:33 -0800

    added s3 support to gemfile

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   has_attached_file :photo, 
                     :styles => {:thumb=> "80x80#", :small  => "640x480>" },
-                    :default_url => "/images/gravatar.jpg"    
+                    :default_url => "/images/gravatar.jpg",    
                     :storage => :s3,
                     :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
                     :path => "/:style/:filename"   
