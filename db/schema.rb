6cbc5df31ce1ba75f2d87c12afe724d349e3a432
RAHUL <rahul@rahul-VirtualBox.(none)>
2010-11-28 16:17:10 -0800

    layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101127004900) do
+ActiveRecord::Schema.define(:version => 20101128191934) do
 
   create_table "comments", :force => true do |t|
     t.string   "body"
