b003f9b3aaeb8cc9cfabb05e360f3727c1af7de1
RAHUL <rahul@rahul-VirtualBox.(none)>
2011-01-19 21:12:34 -0800

    removed page.rb

--- app/models/page.rb
+++
@@ -1,9 +0,0 @@
-class Page < ActiveRecord::Base
-  paginate_alphabetically :by => :name
-  
-  def alphabetical_group(letter = nil)
-     letter ||= first_letter
-     find(:all, :conditions => ["LOWER(#{@attribute.to_s}) LIKE ?", "#{letter.downcase}%"], :order => @attribute)
-   end
-   
-end
