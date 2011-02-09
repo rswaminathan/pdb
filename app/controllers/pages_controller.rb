8bccde0f82bddb93b2c4eb5a3ca974d9235be86a
RAHUL <rahul@rahul-VirtualBox.(none)>
2011-02-09 07:37:09 -0800

    new pics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,6 +22,30 @@ class PagesController < ApplicationController
 			@home_page = params[:page]
 		end
 		@projects =Project.all.sort! {|a,b| -(a.created_at <=> b.created_at)}.paginate :page => params[:page], :per_page => 5
+	
+		p_list = Array.new
+    Project.all.each do |project|
+    p_list += project.kind_list
+    end
+
+    p_count=Hash.new
+      p_list.each do |s|
+      p_count[s] = 0
+      end
+
+    p_count.each do |w|
+      p_list.each do |s|
+    	if (w[0]==s && w[0] != "Enter tags(comma separated)")
+    	p_count[s] +=1
+    	end
+      end
+      end
+
+    p_sorted= p_count.sort {|a,b| -(a[1]<=>b[1])}
+    @links = p_sorted[0,40]
+    
+    
+
 	end
 	
 	def contact
