ffe0df0c8c157e35cd572afb3a104c2eee4360c0
RAHUL <rahul@rahul-VirtualBox.(none)>
2011-02-16 06:13:51 -0800

    only one file attachment

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -13,12 +13,12 @@ module PagesHelper
       end
 
     p_count.each do |w|
-      p_list.each do |s|
-    	if (w[0]==s && w[0] != "Enter tags(comma separated)")
-    	p_count[s] +=1
-    	end
-      end
-      end
+        p_list.each do |s|
+        	if (w[0]==s && w[0] != "Enter tags(comma separated)")
+        	  p_count[s] +=1
+        	end
+        end
+    end
 
     p_sorted= p_count.sort {|a,b| -(a[1]<=>b[1])}
     @links = p_sorted[0,30]
@@ -27,3 +27,42 @@ module PagesHelper
 	
 end
 
+  def User_sorter
+    
+    u_list = Array.new
+    
+		Profile.all.each do |profile|
+		  
+		  if !profile.occupation.nil?
+		    u_list += profile.occupation.to_a
+		  end
+		  
+		  if !profile.year.nil?
+		    u_list += profile.year.to_a
+		  end
+	
+		  if !profile.institution.nil?
+		    u_list += profile.institution.to_a
+		  end	  
+		  
+		  if !profile.department.nil?
+		    u_list += profile.department.to_a
+		  end
+		  
+    end
+
+    p_count=Hash.new
+      p_list.each do |s|
+      p_count[s] = 0
+      end
+
+    p_count.each do |w|
+        p_list.each do |s|
+        	if (w[0]==s && w[0] != "Enter tags(comma separated)")
+        	  p_count[s] +=1
+        	end
+        end
+    end
+
+    p_sorted= p_count.sort {|a,b| -(a[1]<=>b[1])}
+    @links = p_sorted[0,30]
