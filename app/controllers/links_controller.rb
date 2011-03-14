1c8e34c367ebd3102a99e0921fd742444980c3af
RAHUL <rahul@rahul-VirtualBox.(none)>
2011-03-14 05:21:40 -0700

    fiked newlines from textareas -> project description

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,26 +1,12 @@
 class LinksController < ApplicationController
   def create
+      @project = Project.find(params[:project_id])
+      @link = @project.links.build(params[:link])
+      @link.save
+      fix_providers(@project.links)
+      redirect_to(@project)
   end
 
 
-  def string_scraper(string)
-    supported_sites = ["facebook.com","twitter.com","linkedin.com"]
-    list = string.split(' ')
-    return_hash = {}
-    list.each do |link|
-      c=nil
-      supported_sites.each do |i|
-        if link.includes?(i)
-          return_hash[i] = link
-          return_hash["other"].pop if c
-          break
-        elsif !return_hash["other"][-1]==(link)
-          c=1
-          return_hash["other"].push(link)
-        end
-      end
-      return return_hash
-    end
-  end
 end
 
